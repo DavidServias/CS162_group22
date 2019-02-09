80e9f6de2b11fb231e86075fbbdb3b18e7c309e7
Shon Camarlinghi <shon.camarlinghi@gmail.com>
2019-02-08 19:48:17 -0800

    Base class Critter definitions

---
+++ Critter.hpp
@@ -0,0 +1,39 @@
+/*****************************************************************************************************************
+* Author: CS162 group 22 (Kadeem Wyllie, Shon Camarlinghi, David Servias)
+* Date: 2/8/2019
+* Description: declaration for Critter base class
+****************************************************************************************************************/
+
+#ifndef CRITTER_HPP
+#define CRITTER_HPP
+
+#include <ctime>
+#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <tuple>
+
+
+class Critter {
+public:
+	Critter();
+	~Critter();
+	void setAge(int age);
+	void setLoc(int x, int y); //https://stackoverflow.com/questions/321068/returning-multiple-values-from-a-c-function
+	int getAge();
+	int getLoc();
+
+protected:
+	virtual move();
+	virtual breed();
+    virtual die(); 
+
+private:
+	int age_;
+	int location_; //stores x, y coordinates of the critter
+
+}; 
+
+#endif //CRITTER_HPP
+
