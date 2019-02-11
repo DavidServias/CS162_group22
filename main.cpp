70bd9fc1a8c0ee853eac760dd7a3a77acc23e5cb
Shon Camarlinghi <shon.camarlinghi@gmail.com>
2019-02-10 18:44:35 -0800

    Filled basic main

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*****************************************************************************************************************
+* Author: CS162 group 22 
+* Date: 2/10/2019
+* Description: main 
+****************************************************************************************************************/
+
+
+#include <iostream>
+#include "Game.hpp"
+
+
+
+
+int main(int argc, char **argv) {
+	std::cout << "Welcome to And and Doodlebug game designed by group 22 (G22 for short!)" << std::endl;
+	
+
+	return 0;
+}
